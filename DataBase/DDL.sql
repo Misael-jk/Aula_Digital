cf178bf5b0e1d1141a956c2aa2c30e4666afca02
Misael <misaelpiucaet12d1@gmail.com>
2025-06-22 18:43:18 -0300

    Update Base de datos

--- DataBase/DDL.sql
+++ DataBase/DDL.sql
@@ -13,46 +13,6 @@ create table Docentes (
     constraint UQ_Docentes_Email unique (email)
 );
 
--- create table Curso(
---	idCurso tinyint not null auto_increment,
---	anio tinyint not null,
---	division tinyint not null,
--- 	turno varchar(30) not null,
--- 	constraint PK_Curso primary key(idCurso)
--- );
-
--- create table Alumnos (
---     idAlumno smallint not null auto_increment,
---     dni int not null,
---     nombre varchar(40) not null,
---     apellido varchar(40) not null,
---     email varchar(70) not null,
---     curso varchar(30) not null,
---     constraint PK_Alumnos primary key (idAlumno),
---     constraint UQ_Alumnos unique (dni),
---     constraint UQ_Docentes_Email unique (email)
--- );
-
-
--- create table PermisosPrestamo (
---     idPermiso smallint not null auto_increment,
---     idAlumno smallint not null,
---     idDocente smallint not null,
---     fechaPermiso datetime not null,
---     constraint PK_PermisosPrestamo primary key (idPermiso),
---     constraint FK_PermisosPrestamos_Alumnos foreign key (idAlumno) 
---     	references Alumnos(idAlumno),
---     constraint FK_PermisosPrestamos_Docentes foreign key (idDocente) 
---     	references Docentes(idDocente)
--- );
-
-
--- create table Tecnologia (
--- 	idTecnologia tinyint not null auto_increment,
---     programa varchar(50) not null,
---     constraint PK_Tecnologia primary key (idTecnologia)
--- );
-
 
 create table EstadosNotebook (
     idEstadoNotebook tinyint not null auto_increment,
@@ -65,25 +25,17 @@ create table EstadosNotebook (
 create table Notebooks (
     idNotebook tinyint not null auto_increment,
     idEstadoNotebook tinyint not null,
+    disponibleNotebook boolean not null,
     constraint PK_Notebooks primary key (idNotebook),
     constraint FK_Notebooks_EstadosNotebook foreign key (idEstadoNotebook)
     	references EstadosNotebook(idEstadoNotebook)
 );
 
--- create NotebookTecnologia (
--- idNotebook tinyint not null,
--- idTecnologia tinyint not null,
--- constraint PK_NotebookTecnologia primary key(idNotebook, idTecnologia),
--- constraint FK _NotebookTecnologia_Notebooks foreign key(idNotebook)
---		references Notebook(idNotebook),
--- constraint FK_NotebookTecnologia_Tecnologia foreign key(idTecnologia)
--- 		references Tecnologia(idTecnologia)
--- );
 
 create table Carritos (
     idCarrito tinyint not null auto_increment,
     idDocente smallint not null,
-    disponible boolean not null,
+    disponibleCarrito boolean not null,
     constraint PK_Carritos primary key (idCarrito),
     constraint FK_Carritos_Docentes foreign key (idDocente) 
         references Docentes(idDocente)
@@ -100,11 +52,6 @@ create table CarritoNotebooks (
         references Notebooks(idNotebook)
 );
 
--- create table TipoPrestamo (
--- idTipoPrestamo tinyint not null auto_increment,
--- tipoPrestamo varchar(40) not null,
--- constraint PK_TipoPrestamo primary key(idTipoPrestamo)
--- );
 
 create table Prestamos (
     idPrestamo int auto_increment,
@@ -141,3 +88,19 @@ create table PrestamoDetalle (
     constraint FK_PrestamoDetalle_EstadosPrestamo foreign key (idEstadoPrestamo)
     	references EstadosPrestamo(idEstadoPrestamo)
 );
+
+
+-- create table PrestamoDetalleCarrito (
+--     idPrestamo int not null,
+--     idCarrito tinyint not null,
+--     idEstadoPrestamo tinyint not null,
+--     fechaDevolucion datetime,
+--     constraint PK_PrestamoDetalleCarrito primary key (idPrestamo, idCarrito),
+--     constraint FK_PrestamoDetalleCarrito_Prestamos foreign key (idPrestamo) 
+--         references Prestamos(idPrestamo),
+--     constraint FK_PrestamoDetalleCarrito_Carritos foreign key (idCarrito) 
+--         references Carritos(idCarrito),
+--     constraint FK_PrestamoDetalleCarrito_Estado foreign key (idEstadoPrestamo)
+--         references EstadosPrestamo(idEstadoPrestamo)
+-- );
+
